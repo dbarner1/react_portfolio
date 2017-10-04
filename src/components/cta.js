fef6ea640a65e379c854017b340221d1d24e049b
Dave Barner <dbarner@thelevelup.com>
2017-10-04 18:33:25 -0400

    fixed top-alignment for cta

--- src/components/cta.js
+++ src/components/cta.js
@@ -7,7 +7,7 @@ class Cta extends React.Component {
       <div className={ this.props.display } id="CtaWelcome">
         <div id="CtaText">
           <h1 id="CtaTextTop">Front End Development</h1>
-  			  <h1><p>with backend experience.</p></h1>
+  			  <h1 id="CtasubText"><p>with backend experience.</p></h1>
   			  <h3>A React Developer who specializes in Javascript, React, SASS, CSS and Bootstrap and with developing skills of Fullstack ME(R)N solutions: MongoDB, Express, Mongoose, Webpack and node.js  </h3>
         </div>
       </div>
