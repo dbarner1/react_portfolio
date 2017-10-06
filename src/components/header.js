5b19e3a4b7c93ce31ed9408faf03707b114f7c4b
Dave Barner <dbarner@thelevelup.com>
2017-10-06 14:52:29 -0400

    added phone

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom'
 import React from 'react';
+const phoneImage = require('../images/phone.png');
 
 const Header = () => (
+
+
   <header>
     <nav>
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/portfolio'>Portfolio</Link></li>
         <li><Link to='/contact'>Connect</Link></li>
+        <a href="tel+7814679961"><img className='phoneImage' src={ phoneImage } /></a>
       </ul>
     </nav>
   </header>
